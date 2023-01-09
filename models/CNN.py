4f9a72bb42dbadb94a4da9512eeeaaa531ce91ad
williamtheodor <wls@braincapture.dk>
2023-01-09 15:34:47 +0100

    implement model mest

--- models/CNN.py
+++ models/CNN.py
@@ -11,7 +11,7 @@ class Network(LightningModule):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
+        self.conv2_drop = nn.Dropout1d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
 
