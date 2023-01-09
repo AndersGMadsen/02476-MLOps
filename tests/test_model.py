8073ba2ed45ce2d996a6b167af9f18f12e43e043
williamtheodor <wls@braincapture.dk>
2023-01-09 18:39:13 +0100

    add skip tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,8 +4,10 @@ from src.data.data_utils import CorruptMNIST
 from torchvision import transforms
 from models.CNN import Network
 from torchvision import transforms
+import os.path
+import pytest
 
-
+@pytest.mark.skipif(len(os.listdir(_PATH_DATA + '/raw')) < 2, reason="Data files not found")
 def test_model():
     transform = transforms.Compose([transforms.ToTensor(),
                             transforms.Normalize(0.13207851, 0.30989197)])
@@ -18,7 +20,6 @@ def test_model():
 
     model = Network(batch_size, lr, data_dir)
     
-
-    # test shape of model output, use the first sample
+    # Test shape of model output, use the first sample
     assert model(train_dataset[0][0]).shape == nn.Size([1, 10])
-    assert nn.sum(nn.exp(model(train_dataset[0][0]))) == 1
+    assert abs(nn.sum(nn.exp(model(train_dataset[0][0]))).item() - 1.0) < 1e-6
